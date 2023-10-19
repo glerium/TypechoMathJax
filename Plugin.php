47075d74f4a216cb5e0f9d66ca371f1df80ec452
glerium <970656328@qq.com>
2023-10-19 20:36:43 +0800

    Add files

---
+++ Plugin.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * TypechoMathJax
+ *
+ * @package TypechoMathJax
+ * @author glerium
+ * @version 1.0.0
+ */
+class TypechoMathJax_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Archive')->footer = array(__CLASS__, 'footer');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+    {}
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form){
+        $tex_font_size = new Typecho_Widget_Helper_Form_Element_Text("tex_font_size", NULL, NULL, _t('公式字体大小（px）'), _t('0表示默认值'));
+        $tex_font_size->input->setAttribute('pattern', '[0-9]*');
+        $tex_font_size->input->setAttribute('maxlength', '3');
+        $tex_font_size->input->setAttribute('oninput', 'removeLeadingZeros()');
+        $tex_font_size->value = ltrim($tex_font_size->value, '0');
+        $form->addInput($tex_font_size);
+        echo '
+            <script>
+            function removeLeadingZeros() {
+                var elements = document.getElementsByName("tex_font_size");
+                var input = elements[0];
+                var value = input.value;
+                if (value != "0") {
+                    value = value.replace(/^0+/, "");
+                    input.value = value;
+                }
+            }
+            </script>
+        ';
+    }
+
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 输出尾部js
+     *
+     * @access public
+     * @param unknown $footer
+     * @return unknown
+     */
+    public static function footer() {
+        $tex_font_size = Helper::options()->plugin('TypechoMathJax')->tex_font_size;
+        
+        $set_tex_font_size = '';
+        if($tex_font_size) {
+            $set_tex_font_size = sprintf('
+                var elements = document.getElementsByClassName("MathJax");
+                for (var i = 0; i < elements.length; i++) {
+                    elements[i].style.fontSize = "%dpx";
+                }', $tex_font_size);
+        }
+        printf('
+            <script type="text/x-mathjax-config">
+                MathJax.Hub.Config({
+                    tex2jax: {
+                        inlineMath: [ ["$","$"], ["\\(","\\)"]  ],
+                        processEscapes: true,
+                        skipTags: ["script", "noscript", "style", "textarea", "pre", "code"]
+                    }
+                });
+                MathJax.Hub.Queue(function() {
+                    var all = MathJax.Hub.getAllJax(), i;
+                    for(i = 0; i < all.length; i += 1) {
+                        all[i].SourceElement().parentNode.className += " has-jax";
+                    }
+                    %s
+                });
+            </script>
+  
+            <script id="mathjax-js" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+        ', $set_tex_font_size);
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TypechoMathJax
-一款用于Typecho的MathJax插件
+
+一款用于Typecho的MathJax插件，由hexo主题的[mathjax.ejs文件](https://github.com/litten/hexo-theme-yilia/blob/master/layout/_partial/mathjax.ejs)和mutoo大佬开发的[MathJax插件](https://github.com/typecho/plugins/blob/master/MathJax.php)魔改整合而成，可以解决在某些主题下MathJax显示异常的bug。同时加入了调整公式字体的功能。
+
+## 用法
+
+1. 将项目克隆到本地
+
+2. 将TypechoMathJax文件夹复制到Typecho目录下的/usr/plugins文件夹内
+
+3. 在网站后台选择启用TypechoMathJax插件
